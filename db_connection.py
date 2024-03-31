087f4aaa041e9b5d41c52ae6efa16b89f75d7470
jonasolsaker <jonas1907@live.no>
2024-03-31 19:25:35 +0200

    db name change

--- db_connection.py
+++ db_connection.py
@@ -1,6 +1,6 @@
 import pymongo
 
-atlas_url = 'mongodb+srv://598284:NayC8yGabCb6petB@subscription-db.nuxok2i.mongodb.net/'
+atlas_url = 'mongodb://localhost:27017/' #change to correct atlas address when running, since project now is public
 atlas_client = pymongo.MongoClient(atlas_url)
 atlas_db = atlas_client['FrontController']
 
